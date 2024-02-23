cdc940a79a37456da0a1053a03e23c09f18348f0
nicolejackson2 <2415277@dundee.ac.uk>
2024-02-23 13:30:21 +0000

    created all pages, added content to standards + home page

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+/*JS for nav bar*/
 const nav = document.querySelector(".nav"),
     searchIcon = document.querySelector("#searchIcon"),
     navOpenBtn = document.querySelector(".navOpenBtn"),
@@ -21,3 +23,21 @@ const nav = document.querySelector(".nav"),
     navCloseBtn.addEventListener("click", () => {
         nav.classList.remove("openNav");
     });
+
+
+/*JS for collapsible sections on standards pages*/
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
+
+}
